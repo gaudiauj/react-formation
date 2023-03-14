759ee83dd11d259a673e25d9c132dc13941ab44e
Jean Gaudiau <jean@react-formation.fr>
2023-03-14 14:40:46 +0100

    feature: add footer

--- app/routes/__index.tsx
+++ app/routes/__index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "@remix-run/react";
 
 import Navbar from "~/components/navbar/navbar";
+import Footer from "~/components/footer/footer";
 
 export default function Index() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
