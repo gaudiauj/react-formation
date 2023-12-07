e5cb3bcd41f2ad78e7e7d04dfc7d9b9726c20fae
Jean Gaudiau <jean@react-formation.fr>
2023-12-07 12:54:43 +0100

    fix: du fix

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,8 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-export default {
+module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverModuleFormat: "cjs",
 };
