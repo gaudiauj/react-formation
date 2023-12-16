ab90fe06805302ea4b76452eeea95ed1acf38107
Jean Gaudiau <jean@react-formation.fr>
2023-12-16 15:44:05 +0100

    feat: basic html for blog

--- app/routes/blog_.$slug/route.tsx
+++ app/routes/blog_.$slug/route.tsx
@@ -1,8 +1,20 @@
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+  MetaFunction,
+} from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getBlogPageFromSlug } from "~/models/blog.server";
 import { getPageContent } from "~/models/notion.server";
 import Markdown from "markdown-to-jsx";
+import { Heading } from "@chakra-ui/react";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import styles from "./blog.css";
+import { useEffect } from "react";
+
+// Then register the languages you need
+hljs.registerLanguage("javascript", javascript);
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,8 +26,15 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  {
+    rel: "stylesheet",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css",
+  },
+];
+
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
-  console.log(params);
   const blogData = await getBlogPageFromSlug({ slug: params.slug || "" });
   const markdown = await getPageContent({ id: blogData?.id || "" });
   return { markdown };
@@ -23,10 +42,44 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 
 export default function Index() {
   const { markdown } = useLoaderData<typeof loader>();
+  useEffect(() => {
+    console.log("coucou");
+    const isHighlight = document.querySelector(".hljs");
+    if (!isHighlight) {
+      console.log("wesh");
+      hljs.highlightAll();
+    }
+  }, []);
 
   return (
     <div>
-      <Markdown>{markdown}</Markdown>
+      <Markdown
+        options={{
+          overrides: {
+            h1: {
+              component: Heading,
+              props: {
+                as: "h1",
+              },
+            },
+            h2: {
+              component: Heading,
+              props: {
+                as: "h2",
+              },
+            },
+            h3: {
+              component: Heading,
+              props: {
+                as: "h3",
+                size: "md",
+              },
+            },
+          },
+        }}
+      >
+        {markdown}
+      </Markdown>
     </div>
   );
 }
