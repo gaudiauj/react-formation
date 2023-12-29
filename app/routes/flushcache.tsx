475da90f710530771fbe49e43ab8c9af6853441a
Jean Gaudiau <jean@react-formation.fr>
2023-12-29 20:33:08 +0100

    feat: add a button to flush cache

--- app/routes/flushcache.tsx
+++ app/routes/flushcache.tsx
@@ -1,8 +1,9 @@
-import { Link } from "@remix-run/react";
-import { Link as UiLink } from "@chakra-ui/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import isAdmin from "~/utils/isAdmin.server";
 import flushCache from "~/utils/flushCache.server";
+import { myCache } from "~/entry.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,11 +21,22 @@ export const handle = {
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   if (!(await isAdmin(request))) {
-    flushCache();
+    return redirect("/404");
   }
-  return {};
+  const stats = myCache.getStats();
+  return { stats };
+};
+
+export const action = async () => {
+  flushCache();
 };
 
 export default function Index() {
-  return <div>Cache vidé</div>;
+  const { stats } = useLoaderData<typeof loader>();
+  console.log({ stats });
+  return (
+    <Form method="post">
+      <button type="submit"> vider le cache </button>
+    </Form>
+  );
 }
